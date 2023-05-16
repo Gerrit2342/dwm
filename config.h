337fc09a7326d3a97b672c522e14cc66b555cb17
Gerrit Albert <g.albert@posteo.de>
2023-05-16 10:20:17 +0000

    fontawesome laden

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Inconsolata:size=12" };
+static const char *fonts[]          = { "Inconsolata:size=12", "fontawesome:size=12" };
 static const char dmenufont[]       = "Inconsolata:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#005577";
