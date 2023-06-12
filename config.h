a05d846b8b5ca151572a7ea777d6b162942cd07e
Gerrit Albert <g.albert@posteo.de>
2023-06-12 16:34:53 +0000

    Regel für Impress-Präsentatiosnmodus hinzugefügt

--- config.h
+++ config.h
@@ -33,10 +33,11 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "KeePassXC",NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Soffice", "soffice", "Presenting: ", 0,        0,         1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
